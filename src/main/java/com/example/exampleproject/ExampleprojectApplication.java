e97d291ef66533e5e7c807bbefdd60db70f47b73
rockgustavo <grcuritiba@gmail.com>
2024-08-06 09:48:24 -0300

    feat: init do projeto

---
+++ src/main/java/com/example/exampleproject/ExampleprojectApplication.java
@@ -0,0 +1,12 @@
+package com.example.exampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleprojectApplication.class, args);
+	}
+}
